e8a6641c920e46d47a8b032af429fc4610e2b262
Kir Kolyshkin <kolyshkin@gmail.com>
2018-05-29 16:15:05 -0700

    parsePsOutput: add a benchmark
    Note that this benchmark does not reflect the real situation,
    as most of overhead of ContainerTop() comes from these two:
    
     - the kernel side collecting various info and formatting it
       (binary to text conversion mostly) for reading by ps from
       various files in /proc/$PID/
    
     - ps utility doing open/read/close of /proc files, text to
       binary conversion, sorting, binary to text conversion and
       formatting.
    
    Still, this benchmark is somewhat useful as it measures performance
    of the last component in the chain, i.e. parsePsOutput().
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- daemon/top_unix_test.go
+++ daemon/top_unix_test.go
@@ -3,6 +3,9 @@
 package daemon // import "github.com/docker/docker/daemon"
 
 import (
+	"bytes"
+	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -75,6 +78,61 @@ func TestContainerTopParsePSOutput(t *testing.T) {
 		}
 		if !f.errExpected && err != nil {
 			t.Fatalf("expected nil, got %v (%q)", err, string(f.output))
+		}
+	}
+}
+
+func BenchmarkParsePsOutput(b *testing.B) {
+	const (
+		pidMax  = 32768 // as in /proc/sys/kernel/pid_max
+		psLines = 10000 // number of lines returned by ps, must be < pidMax
+		pidNum  = 100   // number of container PIDs, must be < psLines
+	)
+
+	// Generate input data for parsePSOutput()
+	rnd := rand.New(rand.NewSource(42))
+
+	// template of ps output
+	hdr := []byte("UID        PID  PPID  C STIME TTY          TIME CMD\n")
+	line := []byte("kir       #pid# 4952  0 May25 pts/0    00:00:01 bash\n")
+	out := append(hdr, bytes.Repeat(line, psLines)...)
+
+	// container pids
+	pids := make([]uint32, pidNum)
+	var offset int
+	for p := 0; p < pidNum; p++ {
+		pid := uint32(rnd.Intn(pidMax))
+		pids[p] = pid
+		// replace a template by the pid
+		i := bytes.IndexByte(out[offset:], '#')
+		if i < 0 {
+			b.Fatalf("not enough lines for %d pids", pidNum)
+		}
+		offset += i
+		pidStr := fmt.Sprintf("%5d", pid)
+		copy(out[offset:], pidStr)
+	}
+
+	// replace the rest of #pid# placeholders
+	for {
+		i := bytes.IndexByte(out[offset:], '#')
+		if i < 0 {
+			break // no more placeholders
+		}
+		offset += i
+		copy(out[offset:], []byte("44444")) // replacement should be > pidMax
+	}
+
+	// benchmark
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		res, err := parsePSOutput(out, pids)
+		if err != nil {
+			b.Fatal(err)
+		}
+		// check number of lines returned is as expected
+		if len(res.Processes) != pidNum {
+			b.Fatalf("expected %d lines, got %d", pidNum, len(res.Processes))
 		}
 	}
 }
